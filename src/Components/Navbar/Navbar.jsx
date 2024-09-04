85936641c6ceadabbee0e4ff1b49283ffd2766a2
anushka1610-yadav <anushkaydv1610@gmail.com>
2024-09-04 22:51:45 +0530

    created the navbar section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <nav>
-
+    <nav className='container'>
+      <img className='logo' src={logo} alt=''/>
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About Us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li><button className='btn'>Contact Us</button></li>
+      </ul>
     </nav>
   )
 }
