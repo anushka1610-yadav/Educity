db41a690514d8d0c0c9563f1795189020a5cfdca
anushka1610-yadav <anushkaydv1610@gmail.com>
2024-09-07 17:20:24 +0530

    completed the contact component ui

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Title from './Components/Title/Title';
 import About from './Components/About/About';
 import Campus from './Components/Campus/Campus';
 import Testimonials from './Components/Testimonials/Testimonials';
-
+import Contact from './Components/Contact/Contact'
 const App = () => {
   return (
     <div>
@@ -21,6 +21,7 @@ const App = () => {
         <Title subtitle='Testimonials' title='What Student Says'/>
         <Testimonials />
         <Title subtitle='Contact Us' title='Get in Touch'/>
+        <Contact />
       </div>
 
     </div>
